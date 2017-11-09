07d98181f26710cd9acd033b6c7e8f162e8488d8
vsakun <vsakun@gmail.com>
2017-11-09 17:26:16 +0300

    maket

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 function slider(){
 	$(".control .nextBlok").on('click', function(){
-		alert("vpered");
+		var slide = $("#project div[class=row]");
+		var slideHid = $("#project div[class=row hid]");
+		slide.addClass("hid");
 	});
 	$(".control .prevBlok").on('click', function(){
 		alert("nazad");
